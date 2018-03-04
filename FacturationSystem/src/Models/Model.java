e0c4e1578a07d0f05b70e126d35da5fdffb4c86d
Arles96 <arles.cerrato@unitec.edu>
2018-03-03 21:50:18 -0600

    Finalizacion de la clase Model con su documentacion correspondiente

--- FacturationSystem/src/Models/Model.java
+++ FacturationSystem/src/Models/Model.java
@@ -1,28 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
  * @author Arles Cerrato
  */
+
+/**
+ * Clase Padre para los modelos de las entidades fuertes y debiles
+*/
 public abstract class  Model {
     
+    /**
+     * direccion donde esta la base de datos del sistema de facturacion
+     */
+    private final String url = "./database/SistemaFacturacion.db";
+    
+    /**
+     * Atributo para conectarte a la base de  datos
+    */
+    protected Connection connect;
+    
+    /**
+     * Metodo para insertar una tupla. 
+     * @param array: Arreglo donde contendra los datos correspondientes para la inserccion
+    */
     public abstract void insert(Object [] array);
     
+    /**
+     * Metodo para actualizar los datos de una tupla
+     * @param array: donde contendra los datos que se actualizaran
+     */
     public abstract void update(Object[] array);
     
-    public abstract void delete(Object[] array);
+    /**
+     * Metodo para eliminar una tupla.
+     * @param id
+    */
+    public abstract void delete(Object id);
     
-    public void connection(){
-        
+    /**
+     * Metodo que realiza la conexion a la base de datos.
+     * Es obligatorio declararlo para realizar una consulata.
+     * @return retorna un booleano para saber si se realizo la conecccion
+    */
+    protected boolean connect(){
+        try {
+            connect = DriverManager.getConnection("jdbc:sqlite:"+url);
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
     }
     
-    public void desconnect (){
-        
+    /**
+     * Metodo para cerrar la conexion a la base de datos.
+     * Es obligatiorio declararlo al finalizar la consulta
+    */
+    protected void close (){
+        try {
+            connect.close();
+        } catch (SQLException e) {
+            System.out.println("Error al cerrar la base de datos");
+        }
     }
     
 }
