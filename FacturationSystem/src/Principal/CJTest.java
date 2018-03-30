42195b7e2f0b78733c78afb8fbf255332029ddbe
dmendoza3 <dmendoza3@unitec.edu>
2018-03-30 11:12:07 -0600

    Agregados modelos

--- FacturationSystem/src/Principal/CJTest.java
+++ FacturationSystem/src/Principal/CJTest.java
@@ -1,5 +1,8 @@
 package Principal;
 
+import Entities.Provider;
+import Models.Provider_Model;
+
 /**
  *
  * @author CJ
@@ -10,7 +13,9 @@ public class CJTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Provider pro = new Provider("1234", "Diego", "Col Cerro grande", "djosue@gmail.com", "22255571");
+        Provider_Model pm = new Provider_Model();
+        pm.insert(pro);
     }
     
 }
